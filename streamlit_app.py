8d5d064a22366009c44a9670817a5300dd8164b4
Ravikiran Butti <ravikiran763@gmail.com>
2023-06-03 14:14:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,30 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-"""
-Ravi
+def generate_word_cloud(job_description):
+    # Logic to generate word cloud based on job description
+    wordcloud = WordCloud().generate(job_description)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    # Display the word cloud using Matplotlib
+    plt.figure(figsize=(10, 6))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def main():
+    # Set page title and layout
+    st.set_page_config(page_title='InclusionIQ', layout='wide')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Left panel
+    st.sidebar.title('InclusionIQ')
+    job_title = st.sidebar.text_input('Job Title')
+    job_description = st.sidebar.text_area('Job Description')
 
+    # Right panel
+    st.title('Word Cloud')
+    if st.button('Generate Word Cloud'):
+        generate_word_cloud(job_description)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == '__main__':
+    main()
